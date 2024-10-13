8b611d334af73e6c6ba13ed90acde322cd626103
Tanmyname <tanxzxzxzxz@gmail.com>
2024-10-13 21:15:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,5 @@ echo "system :"; pip install colorama
 echo "system :"; pip install requests
 echo "system :"; pip install yt-dlp
 echo "system :"; pip install psutil
-echo "system :"; pip install pygame
 echo "system :"; pip install pytubefix
 echo "system :"; python3 main.py
